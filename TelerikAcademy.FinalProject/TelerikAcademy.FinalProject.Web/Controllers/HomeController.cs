546727e16982c031833f0a3f2d16225f908f1cb9
Camyul <vankata.lt@outlook.com>
2017-09-30 02:37:15 +0300

    Create partial view for Product

--- TelerikAcademy.FinalProject/TelerikAcademy.FinalProject.Web/Controllers/HomeController.cs
+++ TelerikAcademy.FinalProject/TelerikAcademy.FinalProject.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TelerikAcademy.FinalProject.Services.Contracts;
+using TelerikAcademy.FinalProject.Web.Models.Home;
 
 namespace TelerikAcademy.FinalProject.Web.Controllers
 {
@@ -17,7 +18,20 @@ namespace TelerikAcademy.FinalProject.Web.Controllers
         }
         public ActionResult Index()
         {
-            return View();
+            var products = this.productsService
+                .GetAll()
+                .OrderByDescending(x => x.CreatedOn)
+                .Select(x => new ProductViewModel()
+                {
+                    PictureUrl = x.PictureUrl,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Price = x.Price
+                })
+                .Take(8)
+                .ToList();
+
+            return View(products);
         }
 
         public ActionResult About()
---
+++ TelerikAcademy.FinalProject/TelerikAcademy.FinalProject.Web/Models/Home/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelerikAcademy.FinalProject.Web.Models.Home
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
